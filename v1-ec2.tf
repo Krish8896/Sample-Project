54a1536d5fa15ae0547960634d15eae51d8f83ae
Krish8896 <krishna77kicha@gmail.com>
2025-09-20 11:43:20 +0530

    modified

--- v1-ec2.tf
+++ v1-ec2.tf
@@ -41,7 +41,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_incoming" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.demo-aws_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
